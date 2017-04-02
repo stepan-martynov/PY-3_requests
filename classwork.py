533786c30e6ab162151d077cb7915dd440e9f75c
Stepan Martynov <smartd88@gmail.com>
2017-04-02 20:26:33 +0300

    creating fole traslate

--- classwork.py
+++ classwork.py
@@ -1,4 +1,5 @@
 import requests
+import subprocess
 
 API_KEY = "trnsl.1.1.20170402T140528Z.9741d87c67b169b8.498152cdc426431b0befea1aad4cd69c1b358a76"
 URL = "https://translate.yandex.net/api/v1.5/tr.json/translate"
@@ -27,7 +28,8 @@ def yandex_translate(input_file='texts/DE.txt', to_lang='de'):
     return translated_text
 
 
-def put_translated_text_to(output_file, translated_text):
+def create_file_translate(output_file, translated_text):
+    subprocess.run(['touch', output_file])
     with open(output_file, encoding='utf-8', mode='w') as f:
         f.write(translated_text)
 
@@ -37,7 +39,7 @@ def main():
     to_lang = input('Введите с какого языка перводим: ')
     translated_text = yandex_translate(input_file, to_lang)
     output_file = input('Введите имя файла: ')
-    put_translated_text_to(output_file, translated_text)
+    create_file_translate(output_file, translated_text)
 
 
 if __name__ == '__main__':
