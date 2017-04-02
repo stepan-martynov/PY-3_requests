04609f509fd6c276eb334305e8730b704863dfbe
Stepan Martynov <smartd88@gmail.com>
2017-04-02 19:49:02 +0300

    Finished

---
+++ classwork.py
@@ -0,0 +1,44 @@
+import requests
+
+API_KEY = "trnsl.1.1.20170402T140528Z.9741d87c67b169b8.498152cdc426431b0befea1aad4cd69c1b358a76"
+URL = "https://translate.yandex.net/api/v1.5/tr.json/translate"
+
+
+def yandex_translate(input_file='texts/DE.txt', to_lang='de'):
+    """
+    https://translate.yandex.net/api/v1.5/tr.json/translate ? 
+    key=<API-ключ>
+     & text=<переводимый текст>
+     & lang=<направление перевода>
+     & [format=<формат текста>]
+     & [options=<опции перевода>]
+     & [callback=<имя callback-функции>]
+     """
+    with open(input_file, encoding='utf-8') as f:
+        text = f.read()
+
+    params = {
+        "key": API_KEY,
+        "text": text,
+        "lang": '{}-ru'.format(to_lang)
+    }
+
+    translated_text = ''.join(requests.get(URL, params=params).json()['text'])
+    return translated_text
+
+
+def put_translated_text_to(output_file, translated_text):
+    with open(output_file, encoding='utf-8', mode='w') as f:
+        f.write(translated_text)
+
+
+def main():
+    input_file = input('Введите имя исходного файла для перевода: ')
+    to_lang = input('Введите с какого языка перводим: ')
+    translated_text = yandex_translate(input_file, to_lang)
+    output_file = input('Введите имя файла: ')
+    put_translated_text_to(output_file, translated_text)
+
+
+if __name__ == '__main__':
+    main()
